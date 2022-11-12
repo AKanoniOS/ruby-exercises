599863087c86ef4b63804111ee78522c8afb4a5a
Steve Schmeltzer <schmeltzersteve@gmailcom>
2022-11-11 16:13:54 -0900

    update word_list to filter out words less than 5 and greater than 12 characters

--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -2,7 +2,35 @@ class Game
 
   word_list = File.readlines("1000Words.txt")
 
-  secret_word = word_list.sample {|word| word.length > 4 && word.length < 13}
+  pruned_word_list = word_list.filter{|word| word.length > 4 && word.length < 13}
+  secret_word = word_list.sample
+  secret_word.downcase!
+  secret_word_array = secret_word.split("").pop
 
-  puts secret_word
+  moves_remaining = 5
+
+  pp display_array = Array.new(secret_word.length, "_")
+
+  while moves_remaining != 0
+    puts "enter a letter to guess:"
+    guess = gets.chomp
+    guess.downcase!
+    guess_word_array = guess.split("")
+
+    
+    puts moves_remaining
+
+    guess_word_array.each_with_index {
+      |item, index|
+
+    }
+
+    pp secret_word_array
+    pp guess_word_array
+    pp display_array
+
+
+
+    moves_remaining -= 1
+  end
 end
