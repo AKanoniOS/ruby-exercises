a5ce0ed96dbd9811a81ed23ae1c1c1962d5f7aaa
Steve Schmeltzer <schmeltzersteve@gmailcom>
2022-11-14 08:01:10 -0900

    add fixes for replit

--- hangman/display.rb
+++ hangman/display.rb
@@ -4,90 +4,88 @@ class Display
   def initialize(secret_word = '', moves_remaining = 9)
     @secret_word = secret_word
     if moves_remaining == 9
-      @hearts3 = "♥♥♥"
-      @hearts2 = "♥♥♥"
-      @hearts1 = "♥♥♥"
+      @hearts3 = '♥♥♥'
+      @hearts2 = '♥♥♥'
+      @hearts1 = '♥♥♥'
     elsif moves_remaining == 8
-      @hearts3 = "♥♥♥"
-      @hearts2 = "♥♥♥"
-      @hearts1 = "♥♥"
+      @hearts3 = '♥♥♥'
+      @hearts2 = '♥♥♥'
+      @hearts1 = '♥♥ '
     elsif moves_remaining == 7
-      @hearts3 = "♥♥♥"
-      @hearts2 = "♥♥♥"
-      @hearts1 = "♥"
+      @hearts3 = '♥♥♥'
+      @hearts2 = '♥♥♥'
+      @hearts1 = '♥  '
     elsif moves_remaining == 6
-      @hearts3 = "♥♥♥"
-      @hearts2 = "♥♥♥"
-      @hearts1 = ""
+      @hearts3 = '♥♥♥'
+      @hearts2 = '♥♥♥'
+      @hearts1 = '   '
     elsif moves_remaining == 5
-      @hearts3 = "♥♥♥"
-      @hearts2 = "♥♥"
-      @hearts1 = ""
+      @hearts3 = '♥♥♥'
+      @hearts2 = '♥♥ '
+      @hearts1 = '   '
     elsif moves_remaining == 4
-      @hearts3 = "♥♥♥"
-      @hearts2 = "♥"
-      @hearts1 = ""
+      @hearts3 = '♥♥♥'
+      @hearts2 = '♥  '
+      @hearts1 = '   '
     elsif moves_remaining == 3
-      @hearts3 = "♥♥♥"
-      @hearts2 = ""
-      @hearts1 = ""
+      @hearts3 = '♥♥♥'
+      @hearts2 = '   '
+      @hearts1 = '   '
     elsif moves_remaining == 2
-      @hearts3 = "♥♥"
-      @hearts2 = ""
-      @hearts1 = ""
+      @hearts3 = '♥♥ '
+      @hearts2 = '   '
+      @hearts1 = '   '
     elsif moves_remaining == 1
-      @hearts3 = "♥"
-      @hearts2 = ""
-      @hearts1 = ""
+      @hearts3 = '♥  '
+      @hearts2 = '   '
+      @hearts1 = '   '
     elsif moves_remaining == 0
-      @hearts3 = ""
-      @hearts2 = ""
-      @hearts1 = ""
+      @hearts3 = '   '
+      @hearts2 = '   '
+      @hearts1 = '   '
     else
-      @hearts3 = ""
-      @hearts2 = ""
-      @hearts1 = ""
+      @hearts3 = '   '
+      @hearts2 = '   '
+      @hearts1 = '   '
     end
-    
   end
 
   def loselife
-    if @hearts1.include?("♥")
-      @hearts1.slice!("♥")
-      @hearts1 += " "
-    elsif @hearts2.include?("♥")
-      @hearts2.slice!("♥")
-      @hearts2 += " "
+    if @hearts1.include?('♥')
+      @hearts1.slice!('♥')
+      @hearts1 += ' '
+    elsif @hearts2.include?('♥')
+      @hearts2.slice!('♥')
+      @hearts2 += ' '
     else
-      @hearts3.slice!("♥")
-      @hearts3 += " "
+      @hearts3.slice!('♥')
+      @hearts3 += ' '
     end
   end
-  
 
   def title
-    %{
+    %(
     ██   ██  █████  ███    ██  ██████  ███    ███  █████  ███    ██ 
     ██   ██ ██   ██ ████   ██ ██       ████  ████ ██   ██ ████   ██ 
     ███████ ███████ ██ ██  ██ ██   ███ ██ ████ ██ ███████ ██ ██  ██ 
     ██   ██ ██   ██ ██  ██ ██ ██    ██ ██  ██  ██ ██   ██ ██  ██ ██ 
     ██   ██ ██   ██ ██   ████  ██████  ██      ██ ██   ██ ██   ████ 
-    }.red
+    ).red
   end
 
   def main(mr, display_array, alphabet)
     %{
         #{@hearts3.red}
-        #{@hearts2.red}     ________    #{display_array}  (#{display_array.split(" ").length} letter word)
-        #{@hearts1.red}    |        |                                                                     
-               O        |                                                                    
-              /|\\       #{"██   ██  █████  ███    ██  ██████  ███    ███  █████  ███    ██".red}       
-               |        #{"██   ██ ██   ██ ████   ██ ██       ████  ████ ██   ██ ████   ██".red}        
-              / \\       #{"███████ ███████ ██ ██  ██ ██   ███ ██ ████ ██ ███████ ██ ██  ██".red}        
-      Moves             #{"██   ██ ██   ██ ██  ██ ██ ██    ██ ██  ██  ██ ██   ██ ██  ██ ██".red}        
-      Remaining: #{mr.to_s.red}      #{"██   ██ ██   ██ ██   ████  ██████  ██      ██ ██   ██ ██   ████".red} 
+        #{@hearts2.red}     ________    #{display_array}  (#{display_array.split(' ').length} letter word)
+        #{@hearts1.red}    |        |
+               O        |
+              /|\\       #{'██   ██  █████  ███    ██  ██████  ███    ███  █████  ███    ██'.red}
+               |        #{'██   ██ ██   ██ ████   ██ ██       ████  ████ ██   ██ ████   ██'.red}
+              / \\       #{'███████ ███████ ██ ██  ██ ██   ███ ██ ████ ██ ███████ ██ ██  ██'.red}
+      Moves             #{'██   ██ ██   ██ ██  ██ ██ ██    ██ ██  ██  ██ ██   ██ ██  ██ ██'.red}
+      Remaining: #{mr.to_s.red}      #{'██   ██ ██   ██ ██   ████  ██████  ██      ██ ██   ██ ██   ████'.red}
 
-      #{alphabet.join(" ")}
+      #{alphabet.join(' ')}
     }
   end
 
@@ -96,6 +94,6 @@ class Display
   end
 
   def win
-    "YOU WIN!!!".green
+    'YOU WIN!!!'.green
   end
 end
--- hangman/hangman.rb
+++ hangman/hangman.rb
@@ -1,84 +1,80 @@
-require_relative("display.rb")
+require_relative('display.rb')
 require 'tty-prompt'
 require 'yaml'
-
 
 prompt = TTY::Prompt.new
 display = Display.new
 puts display.title
-mode = prompt.select("Would you like to start a new game or resume an old one?", %w(New_game Resume_game))
+mode = prompt.select('Would you like to start a new game or resume an old one?', %w[New_game Resume_game])
 
 class Person
-  def initialize(name = "", age = "")
+  def initialize(name = '', age = '')
     @name = name
     @age = age
   end
 
   attr_reader :name, :age
-
 end
 
 class Game
   def initialize(mode)
     @mode = mode
-    if @mode == "New_game"
-      @word_list = File.readlines("1000Words.txt")
-      @word_list.filter!{|word| (word.length > 4 && word.length < 13)}
+    if @mode == 'New_game'
+      @word_list = File.readlines('1000Words.txt')
+      @word_list.filter! { |word| (word.length > 4 && word.length < 13) }
       @secret_word = @word_list.sample
       @secret_word.downcase!
-      @secret_word_array = @secret_word.split("")
+      @secret_word_array = @secret_word.split('')
       @secret_word_array.pop
-    else 
-      #put code for reading serialization here
+    else
+      # put code for reading serialization here
     end
   end
 
   def play
-    
-    if @mode == "New_game"
+    if @mode == 'New_game'
       display = Display.new(@secret_word)
 
       moves_remaining = 9
 
-      display_array = Array.new(@secret_word.length-1, "_")
+      display_array = Array.new(@secret_word.length - 1, '_')
       fails_array = []
-      display_alphabet = %w( a b c d e f g h i j k l m n o p q r s t u v w x y z )
-      circle_alphabet = %w( ⓐ ⓑ ⓒ ⓓ ⓔ ⓕ ⓖ ⓗ ⓘ ⓙ ⓚ ⓛ ⓜ ⓝ ⓞ ⓟ ⓠ ⓡ ⓢ ⓣ ⓤ ⓥ ⓦ ⓧ ⓨ ⓩ )
-      strike_alphabet = %w( a̶ b̶ c̶ d̶ e̶ f̶ g̶ h̶ i̶ j̶ k̶ l̶ m̶ n̶ o̶ p̶ q̶ r̶ s̶ t̶ u̶ v̶ w̶ x̶ y̶ z̶ )
+      display_alphabet = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
+      circle_alphabet = %w[ⓐ ⓑ ⓒ ⓓ ⓔ ⓕ ⓖ ⓗ ⓘ ⓙ ⓚ ⓛ ⓜ ⓝ ⓞ ⓟ ⓠ ⓡ ⓢ ⓣ ⓤ ⓥ ⓦ ⓧ ⓨ ⓩ]
+      strike_alphabet = %w[a̶ b̶ c̶ d̶ e̶ f̶ g̶ h̶ i̶ j̶ k̶ l̶ m̶ n̶ o̶ p̶ q̶ r̶ s̶ t̶ u̶ v̶ w̶ x̶ y̶ z̶]
 
-      puts "                            "
-      puts "enter letter to guess:"
+      puts '                            '
+      puts 'enter letter to guess:'
 
+      puts display.main(moves_remaining, display_array.join(' '), display_alphabet)
 
-
-      puts display.main(moves_remaining, display_array.join(" "), display_alphabet)
-      
       while moves_remaining != 0
-        
+
         prompt = TTY::Prompt.new
-        guess = prompt.keypress("Enter a letter to guess or press [space] to save game:")
+        guess = prompt.keypress('Enter a letter to guess or press [space] to save game:')
         guess.downcase!
 
-        if guess == display_array.join(" ")
-          puts "You guessed it"
+        if guess == display_array.join(' ')
+          puts 'You guessed it'
           exit
         end
 
         not_found = true
 
-        #if guess is correct
-        @secret_word_array.each_with_index {
-          |item, index|
-          if item == guess
-            display_array[index] = item
-            not_found = false
+        # if guess is correct
+        @secret_word_array.each_with_index do |item, index|
+          next unless item == guess
 
-            # if found, circle letter in display
-            display_alphabet = display_alphabet.each_with_index {|item, index| display_alphabet[index] = circle_alphabet[index] if item == guess}
+          display_array[index] = item
+          not_found = false
+
+          # if found, circle letter in display
+          display_alphabet = display_alphabet.each_with_index do |item, index|
+            display_alphabet[index] = circle_alphabet[index] if item == guess
           end
-        }
+        end
 
-        if guess == " "
+        if guess == ' '
           save = {
             moves_remaining: moves_remaining,
             display_array: display_array,
@@ -87,7 +83,7 @@ class Game
             secret_word_array: @secret_word_array
           }
 
-          File.open('save.yml','w') { |file| file.write(save.to_yaml) }
+          File.open('save.yml', 'w') { |file| file.write(save.to_yaml) }
 
           exit
         end
@@ -95,16 +91,18 @@ class Game
         # if guess is incorrect
         if not_found
           # if not found, block off in display
-          display_alphabet = display_alphabet.each_with_index {|item, index| display_alphabet[index] = "▉" if item == guess}
-          
+          display_alphabet = display_alphabet.each_with_index do |item, index|
+            display_alphabet[index] = '▉' if item == guess
+          end
+
           display.loselife
           moves_remaining -= 1
         end
 
-        puts display.main(moves_remaining, display_array.join(" "), display_alphabet)
+        puts display.main(moves_remaining, display_array.join(' '), display_alphabet)
 
-        #checks if word is filled in
-        if display_array.join("").include?("_") == false
+        # checks if word is filled in
+        if display_array.join('').include?('_') == false
           puts display.win
           exit
         end
@@ -112,7 +110,7 @@ class Game
       end
 
       puts display.reveal
-    else #RESUME GAME
+    else # RESUME GAME
 
       retrieve = YAML.load(File.read('save.yml'))
 
@@ -120,46 +118,45 @@ class Game
       display_array = retrieve[:display_array]
       display_alphabet = retrieve[:display_alphabet]
       @secret_word = retrieve[:secret_word]
-      @secret_word_array = retrieve[:secret_word_array]
+      @secret_word_array = @secret_word.split('')
 
-      display = Display.new(@secret_word)
+      display = Display.new(@secret_word, moves_remaining)
 
-      circle_alphabet = %w( ⓐ ⓑ ⓒ ⓓ ⓔ ⓕ ⓖ ⓗ ⓘ ⓙ ⓚ ⓛ ⓜ ⓝ ⓞ ⓟ ⓠ ⓡ ⓢ ⓣ ⓤ ⓥ ⓦ ⓧ ⓨ ⓩ )
-      strike_alphabet = %w( a̶ b̶ c̶ d̶ e̶ f̶ g̶ h̶ i̶ j̶ k̶ l̶ m̶ n̶ o̶ p̶ q̶ r̶ s̶ t̶ u̶ v̶ w̶ x̶ y̶ z̶ )
+      circle_alphabet = %w[ⓐ ⓑ ⓒ ⓓ ⓔ ⓕ ⓖ ⓗ ⓘ ⓙ ⓚ ⓛ ⓜ ⓝ ⓞ ⓟ ⓠ ⓡ ⓢ ⓣ ⓤ ⓥ ⓦ ⓧ ⓨ ⓩ]
+      strike_alphabet = %w[a̶ b̶ c̶ d̶ e̶ f̶ g̶ h̶ i̶ j̶ k̶ l̶ m̶ n̶ o̶ p̶ q̶ r̶ s̶ t̶ u̶ v̶ w̶ x̶ y̶ z̶]
 
-      puts "                            "
-      puts "enter letter to guess:"
+      puts '                            '
+      puts 'enter letter to guess:'
 
+      puts display.main(moves_remaining, display_array.join(' '), display_alphabet)
 
-
-      puts display.main(moves_remaining, display_array.join(" "), display_alphabet)
-      
       while moves_remaining != 0
-        
+
         prompt = TTY::Prompt.new
-        guess = prompt.keypress("Enter a letter to guess or press [space] to save game:")
+        guess = prompt.keypress('Enter a letter to guess or press [space] to save game:')
         guess.downcase!
 
-        if guess == display_array.join(" ")
-          puts "You guessed it"
+        if guess == display_array.join(' ')
+          puts 'You guessed it'
           exit
         end
 
         not_found = true
 
-        #if guess is correct
-        @secret_word_array.each_with_index {
-          |item, index|
-          if item == guess
-            display_array[index] = item
-            not_found = false
+        # if guess is correct
+        @secret_word_array.each_with_index do |item, index|
+          next unless item == guess
 
-            # if found, circle letter in display
-            display_alphabet = display_alphabet.each_with_index {|item, index| display_alphabet[index] = circle_alphabet[index] if item == guess}
+          display_array[index] = item
+          not_found = false
+
+          # if found, circle letter in display
+          display_alphabet = display_alphabet.each_with_index do |item, index|
+            display_alphabet[index] = circle_alphabet[index] if item == guess
           end
-        }
+        end
 
-        if guess == " "
+        if guess == ' '
           save = {
             moves_remaining: moves_remaining,
             display_array: display_array,
@@ -167,24 +164,26 @@ class Game
             secret_word: @secret_word
           }
 
-          File.open('save.yml','w') { |file| file.write(save.to_yaml) }
-          
+          File.open('save.yml', 'w') { |file| file.write(save.to_yaml) }
+
           exit
         end
 
         # if guess is incorrect
         if not_found
           # if not found, block off in display
-          display_alphabet = display_alphabet.each_with_index {|item, index| display_alphabet[index] = "▉" if item == guess}
-          
+          display_alphabet = display_alphabet.each_with_index do |item, index|
+            display_alphabet[index] = '▉' if item == guess
+          end
+
           display.loselife
           moves_remaining -= 1
         end
 
-        puts display.main(moves_remaining, display_array.join(" "), display_alphabet)
+        puts display.main(moves_remaining, display_array.join(' '), display_alphabet)
 
-        #checks if word is filled in
-        if display_array.join("").include?("_") == false
+        # checks if word is filled in
+        if display_array.join('').include?('_') == false
           puts display.win
           exit
         end
@@ -194,7 +193,6 @@ class Game
       puts display.reveal
 
     end
-
   end
 end
 
